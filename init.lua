447004b79e01b87e4e2035f0dde4bb32664b0dce
Piotr Rybałtowski <1093710+piotrek-r@users.noreply.github.com>
2024-05-13 12:15:44 +0200

    Add christoomey/vim-tmux-navigator plugin

--- init.lua
+++ init.lua
@@ -863,6 +863,9 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'christoomey/vim-tmux-navigator',
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
